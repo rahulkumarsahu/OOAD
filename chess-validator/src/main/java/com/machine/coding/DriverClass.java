8363833069cc1043e11c7887a359c2835a424357
Rahul Kumar <rahulkumar060995@gmail.com>
2022-03-13 11:03:08 +0530

    Initial commit

---
+++ chess-validator/src/main/java/com/machine/coding/DriverClass.java
@@ -0,0 +1,13 @@
+package com.machine.coding;
+
+/**
+ * Hello world!
+ *
+ */
+public class DriverClass {
+
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+    }
+}
